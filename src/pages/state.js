29a2fb21a00b86935a99a1d054e7c1205b4a9014
PHUWARIN MANEERAT <6131305047@lamduan.mfu.ac.th>
2021-07-30 12:00:12 +0700

    State

--- src/pages/state.js
+++ src/pages/state.js
@@ -3,20 +3,22 @@ export const state = () => {
     return (
         <div style={{ margin: 40 }}>
             <h1>State</h1>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;State คือ ข้อมูล หรือ valueต่างๆ ใน component ซึ่งถ้า State มีการเปลี่ยนแปลงจะ
-                ทำให้ component นั้นมีการ Re-render ใหม่ อัตโนมัติ ตามตัวอย่างเป็นการสร้าง State
-                ของเวลาถ้ามีการ refresh เพื่อ get ค่า Date มาใหม่ เวลา ก็จะมีการเปลี่ยนแปลงเนื่องจาก state ถูกเปลี่ยน
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;State คือ ข้อมูล หรือ value ต่างๆ ที่จะถูกสร้างมาเพื่อไว้ใช้ใน
+                component ตัวเอง ซึ่งถ้า State มีการเปลี่ยนแปลงจะทำให้ component นั้นมีการ Re-render ใหม่ อัตโนมัติ
+                ตามตัวอย่างเป็นการสร้าง State ของเวลาถ้ามีการ refresh เพื่อ get ค่า Date มาใหม่ เวลา
+                ก็จะมีการเปลี่ยนแปลงเนื่องจาก state ถูกเปลี่ยน </p>
+
+            <img src={"https://bit.ly/379QEuu"} alt="State" /><br />
+            <center>
+                <h1>Hello, world!</h1>
+                <h2>It is {date.toLocaleTimeString()}.</h2><br />
+            </center>
+
+            <img src={"https://bit.ly/3x7xwIg"} alt={"setState"} /><br />
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                ซึ่งถ้าหากเราต้องการแก้ State ต้องทำใน Component ที่เป็น Class Component ผ่านการ setState
+                ตามภาพ
             </p>
-
-            <img src={"https://bit.ly/379QEuu"} alt="State" />
-
-            <div>
-                <center>
-                    <h1>Hello, world!</h1>
-                    <h2>It is {date.toLocaleTimeString()}.</h2>
-                </center>
-            </div>
-
 
         </div>
     )
